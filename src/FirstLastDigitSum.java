c97271681609adadcfb7b310123135c87428e0f9
Susana Gutierrez <susanagabriela_ap@hotmail.com>
2023-06-24 17:51:41 -0700

    First Last Digit Sum

---
+++ src/FirstLastDigitSum.java
@@ -0,0 +1,33 @@
+public class FirstLastDigitSum {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumFirstAndLastDigit(5));
+    }
+
+    public static int sumFirstAndLastDigit(int number) {
+        int counter = 0;
+        if (number < 0) {
+            return -1;
+        }
+        int sum = 0;
+        do {
+            if (sum == 0) {
+                sum = number%10;
+            } else if (number/10 == 0) {
+                sum += number;
+            }
+            number = number / 10;
+            counter++;
+        } while (number > 0);
+
+        if (counter == 1) {
+            return sum * 2;
+        }
+
+        return sum;
+    }
+
+
+
+}
