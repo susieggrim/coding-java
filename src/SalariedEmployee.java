30ccfe8cf74bd7e5fd87259064321b909c7a684b
Susana Gutierrez <susanagabriela_ap@hotmail.com>
2023-11-29 20:55:14 -0800

    Worker

---
+++ src/SalariedEmployee.java
@@ -0,0 +1,25 @@
+public class SalariedEmployee extends Employee {
+
+    private double annualSalary;
+    private boolean isRetired;
+
+    public SalariedEmployee(String name, String birthDate, String hireDate, double annualSalary) {
+        super(name, birthDate, hireDate);
+        this.annualSalary = annualSalary;
+    }
+
+    @Override
+    public double collectPay() {
+
+        double paycheck = annualSalary / 26;
+        double adjustedPay = (isRetired) ? 0.9 * paycheck : paycheck;
+
+        return (int) adjustedPay;
+    }
+
+    public void retired() {
+        terminate("2022-12-12");
+        isRetired = true;
+    }
+
+}
---
+++ src/WorkerMain.java
@@ -0,0 +1,25 @@
+public class WorkerMain {
+
+    public static void main(String[] args) {
+
+        Employee tim = new Employee("Tim", "1983-11-29", "2020-01-01");
+        System.out.println(tim);
+        System.out.println("Age = " + tim.getAge());
+        System.out.println("Pay = " + tim.collectPay());
+
+        SalariedEmployee susie = new SalariedEmployee("Susie", "1975-12-05", "2020-02-02", 35000);
+        System.out.println(susie);
+        System.out.println("Age = " + susie.getAge());
+        System.out.println("Pay = " + susie.collectPay());
+        System.out.println("Susie's Paycheck = $" + susie.collectPay());
+        susie.retired();
+        System.out.println("Susie's pension check = $" + susie.collectPay());
+
+        HourlyEmployee mary = new HourlyEmployee("Mary", "1970-05-05", "2021-03-03", 15);
+        System.out.println(mary);
+        System.out.println("Mary's Paycheck = $" + mary.collectPay());
+        System.out.println("Mary's Holiday Pay = $" + mary.getDoublePay());
+
+    }
+
+}
