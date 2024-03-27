af4336f36083368e1ea9f98a998b5725a698280f
Susana Gutierrez <susanagabriela_ap@hotmail.com>
2024-03-26 22:13:27 -0700

    carchallenge

--- src/carchallenge/CarMain.java
+++ src/carchallenge/CarMain.java
@@ -9,6 +9,12 @@ public class CarMain {
         Car ferrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS", 15.4, 6);
         runRace(ferrari);
 
+        Car tesla = new ElectricCar("2022 Red Tesla Model 3", 568, 75);
+        runRace(tesla);
+
+        Car ferrariHybrid = new HybridCar("2022 Black Ferrari SF90 Stradale", 16, 8, 8);
+        runRace(ferrariHybrid);
+
     }
 
     public static void runRace(Car car) {
