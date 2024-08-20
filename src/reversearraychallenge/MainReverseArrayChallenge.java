a3f5d3d04da2bb57973717d96866ab01f157cdaf
Susana Gutierrez <susanagabriela_ap@hotmail.com>
2024-08-19 21:51:24 -0700

    Reverse Array Challenge

---
+++ src/reversearraychallenge/MainReverseArrayChallenge.java
@@ -0,0 +1,24 @@
+package reversearraychallenge;
+
+import java.util.Arrays;
+
+public class MainReverseArrayChallenge {
+
+    public static void main(String[] args) {
+
+        reverse(new int[] {1, 2, 67, 7, 2});
+
+    }
+
+    private static void reverse(int[] normalArray) {
+        System.out.println("Before: " + Arrays.toString(normalArray));
+        int [] reverseArray = new int[normalArray.length];
+
+        for (int i = 0; i < normalArray.length; i++) {
+            reverseArray[(normalArray.length - 1) - i] = normalArray[i];
+        }
+
+        System.out.println("After: " + Arrays.toString(reverseArray));
+    }
+
+}
