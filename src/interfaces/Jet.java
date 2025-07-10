f98757ec5bde43b0fc069a0c808c43bd0ca684da
Susana Gutierrez <susanagabriela_ap@hotmail.com>
2025-07-09 21:01:44 -0700

    interfaces 2

--- src/interfaces/Jet.java
+++ src/interfaces/Jet.java
@@ -22,4 +22,9 @@ public class Jet implements FlightEnabled, Trackable {
         System.out.println(getClass().getSimpleName() + "'s coordinates recorded");
     }
 
+    @Override
+    public FlightStages transition(FlightStages stage) {
+        System.out.println(getClass().getSimpleName() + "transitioning");
+        return FlightEnabled.super.transition(stage);
+    }
 }
